618f97db73c5b732fc918e2955698c31d373d509
[dyad] <denilson.blicks@gmail.com>
2025-10-12 18:56:48 -0300

    [dyad] Corrigindo o erro 'Select is not defined' no Seiton Tournament importando o componente Select. - wrote 1 file(s)

--- src/pages/Seiton.tsx
+++ src/pages/Seiton.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"; // Adicionado import do Select
 import { useTodoist } from "@/context/TodoistContext";
 import { TodoistTask } from "@/lib/types";
 import LoadingSpinner from "@/components/ui/loading-spinner";
