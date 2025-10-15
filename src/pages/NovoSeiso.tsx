09e7b2c5f515531f6568bd1145a0bb9c1fffab7c
[dyad] <denilson.blicks@gmail.com>
2025-10-15 10:21:48 -0300

    [dyad] Corrigindo erro de build causado por comentário JSX inválido. - wrote 1 file(s)

--- src/pages/NovoSeiso.tsx
+++ src/pages/NovoSeiso.tsx
@@ -338,7 +338,7 @@ const NovoSeiso = () => {
         {!isLoadingCombined && execucaoState === "finished" && (
           <ExecucaoFinishedState
             originalTasksCount={initialTotalTasks}
-            onStartNewFocus={() => setExecucaoState("initial")} {/* Alterado aqui */}
+            onStartNewFocus={() => setExecucaoState("initial")}
           />
         )}
       </div>
