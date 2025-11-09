b8d7dafaca031f5df2153cea89be85e2e857d771
[dyad] <denilson.blicks@gmail.com>
2025-11-09 18:35:03 -0300

    [dyad] wrote 1 file(s)

--- src/components/eisenhower/ScatterPlotMatrix.tsx
+++ src/components/eisenhower/ScatterPlotMatrix.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo, useRef, useState, useEffect } from "react";
+import React, { useMemo, useRef, useState, useEffect, useCallback } from "react";
 import {
   ScatterChart,
   Scatter,
