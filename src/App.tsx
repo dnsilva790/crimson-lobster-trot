973d7929f1563984a6e8f2d12d94cc06211ef3f0
[dyad] <denilson.blicks@gmail.com>
2025-11-21 19:34:07 -0300

    [dyad] Restoring missing modules - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,24 @@ import { TodoistProvider } from "./context/TodoistContext";
 // Import module pages
 import Eisenhower from "./pages/Eisenhower";
 import Agenda from "./pages/Agenda";
-import Triagem from "./pages/Triagem"; // Importar o novo módulo
+import Triagem from "./pages/Triagem";
+import Seiri from "./pages/Seiri";
+import Seiketsu from "./pages/Seiketsu";
+import Seiketsu5W2H from "./pages/Seiketsu5W2H";
+import Seiso from "./pages/Seiso";
+import NovoSeiso from "./pages/NovoSeiso";
+import Planejador from "./pages/Planejador";
+import MassivePlanner from "./pages/MassivePlanner";
+import FollowUp from "./pages/FollowUp";
+import AIAgentManagerPage from "./pages/AIAgentManagerPage";
+import InternalTasks from "./pages/InternalTasks";
+import ProjectManagement from "./pages/ProjectManagement";
+import CreateProjectManagement from "./pages/CreateProjectManagement";
+import EditProjectManagement from "./pages/EditProjectManagement";
+import ProjectManagementDetail from "./pages/ProjectManagementDetail";
+import CardDatabase from "./pages/CardDatabase";
+import TaskReport from "./pages/TaskReport";
+
 
 const queryClient = new QueryClient();
 
@@ -29,6 +46,23 @@ const App = () => (
               <Route path="/eisenhower" element={<Eisenhower />} />
               <Route path="/agenda" element={<Agenda />} />
               <Route path="/triagem" element={<Triagem />} />
+              <Route path="/seiri" element={<Seiri />} />
+              <Route path="/seiketsu" element={<Seiketsu />} />
+              <Route path="/seiketsu-5w2h" element={<Seiketsu5W2H />} />
+              <Route path="/seiso" element={<Seiso />} />
+              <Route path="/seiso/:taskId" element={<Seiso />} /> {/* Route with param */}
+              <Route path="/novo-seiso" element={<NovoSeiso />} />
+              <Route path="/planejador" element={<Planejador />} />
+              <Route path="/massive-planner" element={<MassivePlanner />} />
+              <Route path="/follow-up" element={<FollowUp />} />
+              <Route path="/ai-agent-manager" element={<AIAgentManagerPage />} />
+              <Route path="/internal-tasks" element={<InternalTasks />} />
+              <Route path="/project-management" element={<ProjectManagement />} />
+              <Route path="/project-management/create" element={<CreateProjectManagement />} />
+              <Route path="/project-management/edit/:projectId" element={<EditProjectManagement />} />
+              <Route path="/project-management/:projectId" element={<ProjectManagementDetail />} />
+              <Route path="/card-database" element={<CardDatabase />} />
+              <Route path="/task-report" element={<TaskReport />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Route>
